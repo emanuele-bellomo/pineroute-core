df4c724594ffd35aedc155fe50b81fafd16097d2
emanuele-bellomo <e.bellomo2005@gmail.com>
2026-07-10 00:53:03 +0200

    minor main.py edit

--- src/main.py
+++ src/main.py
@@ -16,9 +16,9 @@ def setup_logging():
     logger.remove()
     
     # Add a custom handler with detailed formatting
-    logger.add(
-        sys.stdout,
-        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    logger.add(sys.stdout,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | "
+               "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
         level="INFO",
         colorize=True
     )
